ef3d0602c31634460899ab6a41af354f4c8130aa
Juan Sebastian <sebastianobh@gmail.com>
2025-02-19 07:39:01 -0500

    sync

---
+++ estructuras-aula/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace estructuras_aula.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
