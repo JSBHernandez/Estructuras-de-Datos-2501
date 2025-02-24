1d9c1659658e1f9e74e0ed637e98d219a8b4aaf0
Juan Sebastian <sebastianobh@gmail.com>
2025-02-24 09:11:25 -0500

    Sync Cola

---
+++ colas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace Colas {
+
+    class Nodo {
+        public int dato { get; set; }
+        public Nodo siguiente { get; set; }
+    }
+
+    public class Cola {
+        private Nodo frente;
+        private Nodo fin;
+
+        public Cola() {
+            frente = null;
+            fin = null;
+        }
+
+        public void Insertar(int dato) {
+            Nodo nuevo = new Nodo();
+            nuevo.dato = dato;
+            nuevo.siguiente = null;
+            if (frente == null) {
+                frente = nuevo;
+            } else {
+                fin.siguiente = nuevo;
+            }
+            fin = nuevo;
+        }
+
+        public int Eliminar() {
+            if (frente == null) {
+                throw new Exception("Cola vacía");
+            }
+            int dato = frente.dato;
+            frente = frente.siguiente;
+            if (frente == null) {
+                fin = null;
+            }
+            return dato;
+        }
+
+        public void Imprimir() {
+            Nodo actual = frente;
+            while (actual != null) {
+                Console.WriteLine(actual.dato);
+                actual = actual.siguiente;
+            }
+        }
+    }
+
+}
