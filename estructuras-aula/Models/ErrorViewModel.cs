58da9cf23ea74dea5012621152b87218fe53db49
Juan Sebastian <sebastianobh@gmail.com>
2025-02-24 09:12:11 -0500

    Sync

--- estructuras-aula/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace estructuras_aula.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
